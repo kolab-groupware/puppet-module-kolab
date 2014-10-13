8960697d95b8b3c469c6a7bf4138079cc9481986
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2014-10-13 18:00:08 +0200

    Enable the necessary modules

--- manifests/bonnie/wui.pp
+++ manifests/bonnie/wui.pp
@@ -13,6 +13,26 @@ class kolab::bonnie::wui inherits kolab::bonnie {
         require => Yum::Repository["kolab-14-extras-audit"]
     }
 
+    if (!defined(Webserver::Module::Enable["mod_authz_core"])) {
+        @webserver::module::enable { "mod_authz_core": }
+    }
+
+    if (!defined(Webserver::Module::Enable["mod_slotmem_shm"])) {
+        @webserver::module::enable { "mod_slotmem_shm": }
+    }
+
+    if (!defined(Webserver::Module::Enable["mod_socache_shmcb"])) {
+        @webserver::module::enable { "mod_socache_shmcb": }
+    }
+
+    if (!defined(Webserver::Module::Enable["mod_ssl"])) {
+        @webserver::module::enable { "mod_ssl": }
+    }
+
+    if (!defined(Webserver::Module::Enable["mod_unixd"])) {
+        @webserver::module::enable { "mod_unixd": }
+    }
+
     webserver::module::enable { "mod_wsgi":
     }
 
@@ -22,5 +42,12 @@ class kolab::bonnie::wui inherits kolab::bonnie {
         template => "kolab/webserver/sites/bonnie.conf.erb"
     }
 
-    realize(Webserver::Virtualhost["bonnie.${vhost_domain}"])
+    realize(
+            Webserver::Module::Enable["mod_authz_core"],
+            Webserver::Module::Enable["mod_slotmem_shm"],
+            Webserver::Module::Enable["mod_socache_shmcb"],
+            Webserver::Module::Enable["mod_ssl"],
+            Webserver::Module::Enable["mod_unixd"],
+            Webserver::Virtualhost["bonnie.${vhost_domain}"]
+        )
 }
