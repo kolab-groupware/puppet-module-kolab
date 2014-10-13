87b2ab9d1a88910fa18c88b9ae45af7d42d1192b
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2014-10-13 17:17:09 +0200

    Add more webserver module management

--- manifests/bonnie/wui.pp
+++ manifests/bonnie/wui.pp
@@ -1,6 +1,20 @@
 class kolab::bonnie::wui inherits kolab::bonnie {
+    include webserver
+
     package { "bonnie-wui":
         ensure => getvar("kolab::pkg::bonnie_wui_version"),
         require => Yum::Repository["kolab-14-extras-audit"]
     }
+
+    webserver::module::enable { "mod_wsgi":
+        enable => true
+    }
+
+    @webserver::virtualhost { "bonnie.${vhost_domain}":
+        enable => true,
+        certificate => true,
+        template => "kolab/webserver/sites/bonnie.conf.erb"
+    }
+
+    realize(Webserver::Virtualhost["bonnie.${vhost_domain}"])
 }
