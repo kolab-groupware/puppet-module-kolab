5a46b9b009aeb4d051fc55698b7581dc7569ee85
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-01-10 14:39:18 +0100

    Add an initial haproxy manifest and template

---
+++ manifests/haproxy.pp
@@ -0,0 +1,27 @@
+class kolab::haproxy {
+    file { "/etc/haproxy/haproxy.cfg":
+        source => "puppet://$server/private/$environment/kolab/haproxy/haproxy.cfg",
+        require => Package["haproxy"],
+        notify => Service["haproxy"]
+    }
+
+    file { "/etc/haproxy/haproxy.cfg-from-tpl":
+        content => template('kolab/haproxy/haproxy.cfg.erb'),
+        require => Package["haproxy"],
+        notify => Service["haproxy"]
+    }
+
+    package { "haproxy":
+        ensure => installed,
+        notify => Service["haproxy"]
+    }
+
+    service { "haproxy":
+        ensure => running,
+        enable => true,
+        require => [
+            File["/etc/haproxy/haproxy.cfg"],
+            Package["haproxy"]
+        ]
+    }
+}
